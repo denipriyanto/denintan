0991532fe6cf20c303a0bf2e2502a27620bcf918
Deni Priyanto <denpri26@gmail.com>
2025-06-14 17:28:12 +0700

    Fix error ts on getUcapan

--- app/api/getUcapan/route.ts
+++ app/api/getUcapan/route.ts
@@ -1,3 +1,9 @@
+type SheetRow = {
+    c: Array<{
+        v: string | null;
+    } | null>;
+};
+
 export async function GET() {
     const res = await fetch(
         "https://docs.google.com/spreadsheets/d/1jwJmJUFYmvoZdd2ap7jOn2iIJz79NP9nN9ICHQIj4gs/gviz/tq?tqx=out:json"
@@ -5,7 +11,7 @@ export async function GET() {
     const text = await res.text();
 
     const json = JSON.parse(text.substr(47).slice(0, -2)); // remove weird prefix/suffix
-    const rows = json.table.rows.map((row: any) => ({
+    const rows = (json.table.rows as SheetRow[]).map((row) => ({
         timestamp: row.c[0]?.v || "",
         name: row.c[1]?.v || "",
         message: row.c[2]?.v || "",
